944912442a0f3fd69cad17ab6afc7345e44ec6be
Jacek Kaczmarek <infinite.tech.poznan@gmail.com>
2024-01-31 00:02:32 +0100

    feat: init spring boot

---
+++ ksef/src/main/java/com/studia/KsefApp.java
@@ -0,0 +1,11 @@
+package com.studia;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsefApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
